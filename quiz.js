ddb51f70fff31a2dddaa0079d5aab3ffc6dfa93b
EKomin220 <ifa.komin@gmail.com>
2025-04-15 15:44:23 +0100

    style with tailwind and add questions

---
+++ quiz.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+     question: "Which of the following is not a fruit?",
+     answers: ["Avocado", "Olives","Broccoli","Pumpkin"],
+     correctIndex: 2
+    },
+    {
+     question:"Which country is the biggest producer of potatoes in the world?",
+     answers: ["China", "Ireland", "India", "USA"],
+     correctIndex: 0
+    },
+   {
+     question: "The biggest tomato (according to Guinness World Records) was grown in Minnesota, USA in 2022. How much did it weigh?",
+     answers: ["2.568 kg", "4.021 kg", "5.284 kg", "7.192 kg"],
+     correctIndex: 2
+   },
+    {
+     question:"Which fruit is also known as the Chinese gooseberry?",
+     answers: ["Honeydew melon", "Kiwi fruit", "Greengage", "Lychee"],
+     correctIndex: 1
+    },
+    {
+      question:"Where did coffee originate from?",
+      answers: ["Brazil", "Ethiopia", "Turkey", "Java"],
+      correctIndex: 1
+    }
+    /*{
+     question:"Which plant is used to make tequila?",
+     answers: ["Sugarcane", "Aloe", "Agave", "Grapes"],
+     correctIndex: 2
+      }*/
+   ]
