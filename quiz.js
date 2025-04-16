2e3510cf192aaac0956df80d8fec0cf3c70dca92
EKomin220 <ifa.komin@gmail.com>
2025-04-16 09:18:06 +0100

    start quiz and question container added

--- quiz.js
+++ quiz.js
@@ -1,4 +1,4 @@
-const questions = [
+const quizQuestions = [
     {
      question: "Which of the following is not a fruit?",
      answers: ["Avocado", "Olives","Broccoli","Pumpkin"],
@@ -24,9 +24,43 @@ const questions = [
       answers: ["Brazil", "Ethiopia", "Turkey", "Java"],
       correctIndex: 1
     }
-    /*{
-     question:"Which plant is used to make tequila?",
-     answers: ["Sugarcane", "Aloe", "Agave", "Grapes"],
-     correctIndex: 2
-      }*/
-   ]
+   ];
+
+   let currentQuestionIndex = 0;
+   let currentQuestion = quizQuestions[currentQuestionIndex];
+   let score = 0;
+   let quizStarted = false;
+
+   const startButton = document.getElementById("start-button");
+   const quizContent = document.getElementById("quiz-content");
+   const questionContainer = document.getElementById("question-container");
+   const result = document.getElementById("result");
+   
+  startButton.addEventListener('click', quizBegin);
+
+  function quizBegin() {
+    quizStarted = true;
+    startButton.classList.add("hidden");
+    quizContent.classList.remove("hidden");
+    showQuestion();
+  }
+
+  function showQuestion (){
+    if (currentQuestionIndex === quizQuestions.length-1){
+        endQuiz();
+        return;
+    }
+  }
+
+    questionContainer.innerHTML = `
+      <div class="question active">
+        <h2 class="text-lg font-medium font-semibold m-5 text-white"> Q ${currentQuestionIndex+1}/${quizQuestions.length}: </h2>
+        <p class="text-md m-5 text-white">${currentQuestion.question}</p>
+        <div class="space-y-5">
+          ${currentQuestion.answers.map((answer, i) => `
+            <button class="option-btn w-full text-left bg-yellow-50 hover:bg-yellow-200 text-black font-medium p-5 rounded-md transition duration-500 ease-in-out" 
+            onclick="checkAnswer(${i})">${answer}</button>`)
+          .join('')}
+         </div>
+      </div>`;
+  
